f1f636d58b9ab8be94ec34858f3297051e8331c7
callajona <156592682+callajona@users.noreply.github.com>
2024-06-25 15:01:21 +0100

    Create connectFourPrototype.h

---
+++ connectFourPrototype.h
@@ -0,0 +1,12 @@
+#define _CONNECTFOURPROTOTYPE_H_
+#ifdef _CONNECTFOURPROTOTYPE_H_
+
+// Function Prototypes
+void displayGrid();
+void displayColNumbers();
+void displayGame();
+int selectCol();
+void placeCounter(int player);
+int winCheck(int player);
+
+#endif
