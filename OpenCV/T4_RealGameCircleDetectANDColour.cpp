2ecadb81a9abf009d5ab6e6e4bc579b4d350f692
callajona <156592682+callajona@users.noreply.github.com>
2024-07-09 11:51:33 +0100

    Update T4_RealGameCircleDetectANDColour.cpp
    Sort function and displaying the game to the terminal now both work fully

--- OpenCV/T4_RealGameCircleDetectANDColour.cpp
+++ OpenCV/T4_RealGameCircleDetectANDColour.cpp
@@ -13,12 +13,14 @@ using namespace cv;
 bool sortYCoord(const Vec3f &a, const Vec3f &b);
 bool sortXCoord(const Vec3f &a, const Vec3f &b);
 
-// Get image: rpicam-jpeg --output /home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour4.jpg
+// Get image: rpicam-jpeg --output /home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour6.jpg
 // Image1: /home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour.jpg
 // Image2: /home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/move4.png
 
-int main(int argc, char** argv)
+int main(int argc, char** argv) 
 {
+    int gameState[42]; // Array to store the game state
+
     // Colour Detection Variables
     Vec3b pixelColour;
     int blue;
@@ -26,21 +28,18 @@ int main(int argc, char** argv)
     int red;
 
     // Import image into code - ENSURE PATH IS CHANGED BEFORE USE
-    Mat img = imread("/home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour4.jpg");
+    Mat img = imread("/home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour6.jpg");
     Mat gray; // Create gray to hold grayscale image
     
     // Check if image is found
-    if (!img.data)
-    {
+    if (!img.data) {
         cout << "No image data\n" << endl;
         return -1;
     }
 
     // Image Processing
-    float scaleFactor = 0.5;
-    int up_width = img.cols * scaleFactor;
-    int up_height = img.rows * scaleFactor;
-    resize(img, img, Size(up_width, up_height),0,0, INTER_LINEAR);
+    float SF = 0.5; // Scaling factor
+    resize(img, img, Size(), SF, SF, INTER_LINEAR); // Resize image by scaling factor
     cvtColor(img, gray, COLOR_BGR2GRAY); // Convert img to grayscale
     //GaussianBlur( gray, gray, Size(9, 9), 2, 2 ); // smooth it, otherwise a lot of false circles may be detected
 
@@ -53,20 +52,18 @@ int main(int argc, char** argv)
     cout << "Number of Circles: " << circles.size() << endl; // Output number of circles
 
     // Sort Circles Vector in grid order
-    sort(circles.begin(), circles.end(), sortYCoord);
-    
+    sort(circles.begin(), circles.end(), sortYCoord); // Sort all vectors based on y-coordinates (rows still scrambled: e.g. 3 2 1; 4 6 5; 9 7 8)
     for (int i = 0; i < 6; i++) {
-        sort(circles.begin() + (i*7), circles.begin() +7 + (i*7),sortXCoord);
+        sort(circles.begin() + (i*7), circles.begin() +7 + (i*7),sortXCoord); // Sort each row
     }
 
-    for(size_t i = 0; i < circles.size(); i++)
-    {
+    for(size_t i = 0; i < circles.size(); i++) {
         // Get Coordinates from Circles vector
         float x = cvRound(circles[i][0]);
         float y = cvRound(circles[i][1]);
-        cout << "(" << x << "," << y << ")\n"; // Display Coordiatnes
+        //cout << "(" << x << "," << y << ")\n"; // Display Coordiatnes
         
-        // Calculate centre point and find radius
+        // Calculate centre point and find radius from vector
         Point center(x,y);
         int radius = cvRound(circles[i][2]);
 
@@ -75,29 +72,41 @@ int main(int argc, char** argv)
         blue = pixelColour[0];
         green = pixelColour[1];
         red = pixelColour[2];
-        //cout << "B: " << blue << "  G: " << green << "  R: " << red << endl; // Display colours
+        //cout << i << " - B: " << blue << "  G: " << green << "  R: " << red << endl; // Display colours
         
         // --- Draw circles on image ---
         circle( img, center, radius, Scalar(255,0,0), 3, 8, 0 ); // draw the circle outline
 
         // Draw centre cirlce - Change colour depending on colour detected
-        if (red > 187 && blue < 200 && green < 200) {
-            circle( img, center, i, Scalar(0,0,255), -1, 8, 0 ); // draw the circle center - red when counter is red
+        if (red > 130 && blue < 100 && green < 100) {
+            circle( img, center, 10, Scalar(0,0,255), -1, 8, 0 ); // draw the circle center - red when counter is red
+            gameState[i] = 1;
         }
-        else if (red > 200 && blue < 200 && green > 200) {
-            circle( img, center, i, Scalar(0,255,255), -1, 8, 0 ); // draw the circle center - yellow when counter is yellow
+        else if (red > 170 && blue < 100 && green > 175) {
+            circle( img, center, 10, Scalar(0,255,255), -1, 8, 0 ); // draw the circle center - yellow when counter is yellow
+            gameState[i] = 2;
         }
         else {
-            circle( img, center, i, Scalar(0,0,0), -1, 8, 0 ); // draw the circle center - Black when no counter
+            circle( img, center, 10, Scalar(0,0,0), -1, 8, 0 ); // draw the circle center - Black when no counter
+            gameState[i] = 0;
         }        
     }
 
-    // Create window to display imagerpicam-jpeg --output /home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/connectFour3.jpg
+    // Display Game State in Terminal
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 7; j++) {
+            cout << gameState[j+(i*7)] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+
+    // Create window to display image
     namedWindow( "circles", 1 );
     imshow( "circles", img );
 
     // Save image
-    //imwrite("/home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/circleDectect_RealGame_WhiteBackgrond.jpg",img); 
+    //imwrite("/home/project/projects/ConnectFourProject/OpenCV/my_cpp_project/src/Images/circleDectect_RealGame_6.jpg",img); 
 
     waitKey(0);
     return 0;
@@ -110,6 +119,3 @@ bool sortYCoord(const Vec3f &a, const Vec3f &b) {
 bool sortXCoord(const Vec3f &a, const Vec3f &b) {
     return a[0] < b[0];
 }
-
-// Sort all y in order
-// Sort X in groups of 7
