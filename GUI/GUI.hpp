3767e7da2700998511cdca6053a066abbf17cd72
callajona <156592682+callajona@users.noreply.github.com>
2024-08-12 17:57:27 +0100

    Update GUI.hpp

--- GUI/GUI.hpp
+++ GUI/GUI.hpp
@@ -3,8 +3,9 @@
 
 #include <opencv2/opencv.hpp>
 
-int GUI_game(int pascal_col, bool Pascal_move, bool display_scores, std::vector<int> scores, std::vector<int> colRank);
-int GUI_difficulty_sel(int diff_sel);
-
+int GUI_game(int pascal_col, bool Pascal_move, bool display_scores, std::vector<int> scores, std::vector<int> colRank); // Display game with different elemets
+int GUI_difficultySel_Screen(int diff_sel); // Display screen for difficulty selection
+int GUI_selectDifficulty(); // Use FSM to select a difficulty
+void blank_backgroud();
 
 #endif
